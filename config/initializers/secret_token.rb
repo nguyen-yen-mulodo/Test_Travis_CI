8a03aab3d0d628e388d8f2ff85e21ea2cb3d9465
haiyen <nguyen.yen@mulodo.com>
2013-09-10 11:37:54 +0700

    ywn ah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minhminh::Application.config.secret_key_base = '52f62243de3c60463accc9e3935649815efa9e1af83530209de44db28d0d0a67d7a51d19c6f10fb7211a74e404500ef131ec08cd7ba4dc130c2fd413cee59b50'
